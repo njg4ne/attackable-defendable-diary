e42b9d74d4c88287a656266ccd06f1ee8c49470a
Nicholas <njg4ne@virginia.edu>
2025-12-03 16:50:50 -0500

    move inline css and js to fetched scripts

---
+++ index.js
@@ -0,0 +1,23 @@
+// https://developer.mozilla.org/en-US/docs/Web/Security/Attacks/XSS
+const params = new URLSearchParams(window.location.search);
+const user = params.get("user");
+const welcome = document.querySelector("#welcome");
+if (Boolean(user)) {
+  const toInsert = user;
+  welcome.innerHTML = `Welcome back, ${toInsert}!`;
+}
+document.getElementById("xss-example").textContent =
+  "?user=You<img hidden src='/fake-endpoint' onerror=alert(1);>";
+
+// basic app functionality
+const STORAGE_KEY = "diaryContent";
+const diaryForm = document.getElementById("diaryForm");
+function onInput(inputEvent) {
+  window.localStorage.setItem(STORAGE_KEY, inputEvent.target.value);
+}
+function onPageLoad() {
+  if (!diaryForm.diary) return;
+  diaryForm.diary.value = window.localStorage.getItem(STORAGE_KEY) ?? "";
+  diaryForm.diary.addEventListener("input", onInput);
+}
+window.addEventListener("load", onPageLoad);
